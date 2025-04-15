d18b9c231959010bd59955b46e9d0a1ca3eabba3
Climbby <franciscoaf.gaming@gmail.com>
2025-04-15 21:12:14 +0000

    fixed thread beggining message and cleaned startGame function

--- commands/game/selections/kitSelection.py
+++ commands/game/selections/kitSelection.py
@@ -6,7 +6,7 @@ from commands.game.startGames import startGame
 
 class KitsButton(discord.ui.View):
     def __init__(self, player, owner):
-        super().__init__(timeout=120)
+        super().__init__(timeout=300)
         self.player = player
         self.owner = owner
         
@@ -19,6 +19,13 @@ class KitsButton(discord.ui.View):
             )
             btn.callback = self.handleSelection
             self.add_item(btn)
+        
+    async def on_timeout(self):
+        for item in self.children:
+            item.disabled = True
+        await self.message.edit(view=self)
+        await asyncio.sleep(3600)
+        await self.message.edit(view=None)
 
     async def handleSelection(self, interaction: discord.Interaction):
         from commands.commands import create_stats_embed
@@ -49,7 +56,7 @@ class KitsButton(discord.ui.View):
             if kit["label"] == kit_type:
                 stats_embed = create_stats_embed(self.player, self.owner.display_name, color=kit["color"])
                 await thread.send(content=f"# __{kit["label"]}__ {kit["emoji"]} Activated!", embed=stats_embed)
-                await asyncio.sleep(2)
+                await asyncio.sleep(1)
                 await thread.send("_(you can do /mystats during the game)_")
         
         # Disable all buttons after selection
--- commands/game/startGames.py
+++ commands/game/startGames.py
@@ -2,15 +2,29 @@ import commands.game.gameLogic as gameLogic
 import asyncio
 
 active_games = {}
+GAME_TICK_INTERVAL = 2
+SELF_DESTRUCT_DELAY = 60
 
 async def startGame(thread, user):
+    """Start and manage a game session in a thread."""
+    try:
+        game = gameLogic.TheBridgeGame(thread, user)
+        active_games[thread.id] = game
+        
+        # Run game loop
+        await run_game_loop(game)
 
-    active_games[thread.id] = gameLogic.TheBridgeGame(thread, user)
-    game = active_games.get(thread.id)
+        # After game ends
+        await thread.send("⏳ The game has ended and will self-destruct in a minute...")
+        await asyncio.sleep(SELF_DESTRUCT_DELAY)
+        
+    finally:
+        # Ensure cleanup happens even if the game crashes
+        active_games.pop(thread.id)
+        await thread.delete()
 
-    while (game.state["myNexusHP"] > 0 and game.state["enemyNexusHP"] > 0):
-        await asyncio.sleep(3)
-        await game.passTime() ########################################## DO THE GAME!!!!!!!!!!
-    await thread.send("⏳ This game thread will self-destruct in a minute...")
-    await asyncio.sleep(60)
-    await thread.delete()
+async def run_game_loop(game: gameLogic.TheBridgeGame):
+    """Run the game loop until one nexus is destroyed."""
+    while game.state["myNexusHP"] > 0 and game.state["enemyNexusHP"] > 0:
+        await asyncio.sleep(GAME_TICK_INTERVAL)
+        await game.passTime()
